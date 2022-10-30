789e0b169b343390b9a13f1ba8a14a86b0bf784c
NikoDimitrov1991 <ndnikolaydimitrov@gmail.com>
2022-10-30 21:12:38 +0200

    Refactor: Code is formatted

--- src/BS.java
+++ src/BS.java
@@ -1,6 +1,18 @@
 public class BS {
-    int binarySearch(int[] arr, int leftSide, int rightSide, int numberWeAreLookingFor){
-        if (rightSide >=leftSide) {
+    public static void main(String[] args) {
+        BS binary = new BS();
+        int[] arr = {2, 10, 11, 34, 80, 99};
+        int x = 99;
+        int result = binary.binarySearch(arr, 0, arr.length - 1, x);
+        if (result == -1) {
+            System.out.println("Element not present");
+        } else {
+            System.out.println("Element present at index    " + result);
+        }
+    }
+
+    int binarySearch(int[] arr, int leftSide, int rightSide, int numberWeAreLookingFor) {
+        if (rightSide >= leftSide) {
             int middleOfList = leftSide + (rightSide - leftSide) / 2;
 
             //If the element is present at the middle itself
@@ -15,17 +27,5 @@ public class BS {
         }
         //If the element is not present in the array we return -1
         return -1;
-    }
-
-    public static void main(String[] args) {
-        BS binary = new BS();
-        int[] arr = {2,10,11,34,80,99};
-        int x = 99;
-        int result = binary.binarySearch(arr,0,arr.length-1,x);
-        if (result == -1) {
-            System.out.println("Element not present");
-        } else {
-            System.out.println("Element present at index    " + result);
-        }
     }
 }
